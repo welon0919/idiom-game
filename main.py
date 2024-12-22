694d4bce8184857d804782bd859e40ee72e18d34
welon0919 <welon@gmail.com>
2024-12-22 20:42:18 +0800

    Added a death animation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 import json
 import sys
 import os
+import threading
+from time import sleep
 WINDOW_WIDTH = 650
 WINDOW_HEIGHT = 800
 
@@ -36,7 +38,7 @@ class SnakeGame:
         self.FONT_SIZE = 25
               # 食物文字顏色
         # 初始化主視窗
-        self.root = root
+        self.root: tk.Tk = root
         self.root.title("貪吃蛇遊戲")
         self.score_display = tk.Label(font=("Arial",20),height=1)
         self.idiom_display = tk.Label(font=("微軟正黑體",25),height=2)
@@ -84,7 +86,7 @@ class SnakeGame:
             x, y = segment
             self.canvas.create_rectangle(
                 x, y, x + self.SNAKE_SIZE, y + self.SNAKE_SIZE,
-                fill=self.SNAKE_COLOR, tag="snake"
+                fill=self.SNAKE_COLOR, tag=("snake",f"{x},{y}")
             )
     
     def spawn_food(self,character):
@@ -114,6 +116,7 @@ class SnakeGame:
             self.direction = new_direction
 
     def move_snake(self):
+        
         head_x, head_y = self.snake[0]
         if self.direction == "Up":
             head_y -= self.SNAKE_SIZE
@@ -134,6 +137,7 @@ class SnakeGame:
             or new_head in self.snake
         ):
             self.running = False
+            self.game_over()
             return
 
         # 更新蛇身
@@ -148,9 +152,9 @@ class SnakeGame:
                 self.score += 1
         else:
             self.snake.pop()
+     
 
     def run_game(self):
-
         if self.running:
             self.direction_updated = True
             self.move_snake()
@@ -158,18 +162,13 @@ class SnakeGame:
             self.score_display.config(text=f"Score : {self.score}")
             self.root.after(100, self.run_game)
         else:
-            self.game_over()
+            pass
 
     def game_over(self):
+        self.canvas.delete("food")
         self.game_overed = True
-        self.canvas.create_text(
-            self.GAME_WIDTH // 2, self.GAME_HEIGHT // 2,
-            text=f"Game Over!", fill="white", font=("Arial", 60),tags="game_over_text"
-        )
-        self.canvas.create_text(
-            self.GAME_WIDTH // 2, self.GAME_HEIGHT // 2+100,
-            text=f"Press space to restart!", fill="white", font=("Arial", 20),tags="game_over_text"
-        )
+        self.kill_snake(-1)
+        
     def spawn_idiom(self):
         self.idiom, self.idiom_meaning = self.get_random_idiom()
         for character in self.idiom:
@@ -187,21 +186,42 @@ class SnakeGame:
             self.reset_game()
             self.run_game()
     def start(self):
+
         if not self.started:
             self.started = True
             self.canvas.delete("start_text")
             self.root.bind("<space>",self.restart_game)
             self.run_game()
-                
-
+    def show_game_over(self):
+        self.canvas.create_text(
+            self.GAME_WIDTH // 2, self.GAME_HEIGHT // 2,
+            text=f"Game Over!", fill="white", font=("Arial", 60),tags="game_over_text"
+        )
+        self.canvas.create_text(
+            self.GAME_WIDTH // 2, self.GAME_HEIGHT // 2+100,
+            text=f"Press space to restart!", fill="white", font=("Arial", 20),tags="game_over_text"
+        )
+    def kill_snake(self,index):
+        self.running = False
+        
+        if index < len(self.snake):
+            
+            body = self.snake[index]
+            tag = f"{body[0]},{body[1]}"
+            self.canvas.delete(tag)
+            self.canvas.update()
+            self.root.after(50,self.kill_snake,index+1)
+        else:
+            self.show_game_over()
+            self.running = False
 # 啟動遊戲
 
 def start_game(root):
     game = SnakeGame(root)
     root.mainloop()
     
-
 if __name__ == "__main__":
+
     root = tk.Tk()
     root.geometry('+0+0')
     root.iconbitmap(get_path("snake.ico"))
